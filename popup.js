f9be74e895d8b0933e02bab56671c51456b56c24
Mahmudur Rahman <nahidacm@gmail.com>
2019-04-03 09:06:53 +0600

    Fully working for Amazon

--- popup.js
+++ popup.js
@@ -1,14 +1,39 @@
-let changeColor = document.getElementById('changeColor')
+// Runs script when popup is opened
+chrome.tabs.executeScript({file: 'contentScript.js'}, function(result) {
+  const product_data = result[0]
+  
+  //Title
+  let title = document.querySelector('#title p')
+  title.innerHTML = product_data.title
+  title.addEventListener('click',function(){
+    navigator.clipboard.writeText(product_data.title)
+  })
 
-chrome.storage.sync.get('color', function(data){
-  changeColor.style.backgroundColor = data.color
-  changeColor.setAttribute('value',data.color)
-})
+  //Short Description
+  let short_desc = document.querySelector('#short_desc p')
+  short_desc.innerHTML = product_data.short_desc
+  short_desc.addEventListener('click',function(){
+    navigator.clipboard.writeText(product_data.short_desc)
+  })
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.executeScript( {file: 'contentScript.js'},function(result){
-    console.log(result)
+  //Technical Specification
+  let tech_spec = document.querySelector('#tech_spec p')
+  tech_spec.innerHTML = product_data.tech_spec
+  tech_spec.addEventListener('click',function(){
+    navigator.clipboard.writeText(product_data.tech_spec)
+  })
+
+  //Images
+  imageRender(product_data.images)
+});
+
+function imageRender(srcArray){
+  srcArray.forEach(src => {
+    let newImg = document.createElement('img')
+    newImg.src = src
+    newImg.addEventListener('click',function(){
+      chrome.downloads.download({url: newImg.src})
+    })
+    document.querySelector('#images p').appendChild(newImg)
   });
-};
-
+}
